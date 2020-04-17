a6cd0126a43c806e02d1df58470220e0fddd453d
Abel Ponce <vnponce8@gmail.com>
2020-04-16 20:32:37 -0500

    first approach getting data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
+const GET_PRODUCTS = gql`
+  {
+    products {
+      id,
+      title,
+      image_url,
+      price(currency: USD)
+    }
+  }
+`;
+
+/**
+ * @return {string}
+ */
 function App() {
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+  if(loading) return 'Loading...';
+  console.log('data =>', data);
   return (
     <div className="App">
       <header className="App-header">
